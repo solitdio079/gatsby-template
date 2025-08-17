82eef0e32c5ef799bfeb4471d7d4aa94042a8f30
diawara09 <diawaraholding223@gmail.com>
2025-08-17 21:57:16 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-template`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
